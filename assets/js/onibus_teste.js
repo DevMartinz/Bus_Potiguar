13b94fb1b400c000af2ea8a7c86bc907357b34fd
Dev_Martinz <Martiinz.742@gmail.com>
2023-07-08 15:43:44 -0300

    Rota e Relacionamentos

--- assets/js/onibus_teste.js
+++ assets/js/onibus_teste.js
@@ -2,18 +2,18 @@ var URL_BASE = "http://localhost:8080/"
 
 function updateList(){
 
-    $.ajax(URL_BASE+"onibus",{
+    $.ajax(URL_BASE+"rota",{
         method:'get',
     }).done(function(res) {
 
-        console.log(res._embedded.onibus);
+        console.log(res._embedded.rota);
 
         let select = $('#sel-linha');
         select.html("");
 
-        $(res._embedded.onibus).each(function(k,el){
-            let onibus = el;
-            let option =  $('<option></option>').attr('value', onibus.numOnibus).text(onibus.numOnibus);
+        $(res._embedded.rota).each(function(index,el){
+            let rota = el;
+            let option =  $('<option></option>').attr('value', index).text(rota.nomeRota);
             select.append(option);
         })
        
@@ -31,12 +31,27 @@ $(function(){
 
     //Sempre que carregar a página atualiza a lista
     updateList();
+
+    $('#rotas-sel').change(function() {
+        var select = $(this).val();
+        var rotaSelecionada = parseInt(select) + 1;
+    });
+
+    $('#btn-buscar').click(function(e) {
+        e.preventDefault(); // Impede o comportamento padrão do link
+    
+        var rotaSelecionada = $('#sel-linha').val();
+        var select = $(this).val();
+        var rotaSelecionada = parseInt(select) + 1;
+        var url = 'rotas.html?opc=' + rotaSelecionada;
+        window.location.href = url;
+      });
 });
 
 function save(){
 
     //envia para o backend
-    $.ajax(URL_BASE+"onibus",{
+    $.ajax(URL_BASE+"rota",{
 
     }).done(function(res) {
         console.log(res);
---
+++ assets/js/rotas.js
@@ -0,0 +1,79 @@
+var URL_BASE = "http://localhost:8080/";
+
+$(function(){
+    //Sempre que carregar a página atualiza a lista
+    selectList();
+    $('#rotas-sel').change(function() {
+        var select = $(this).val();
+        console.log(select);
+        var rotaSelecionada = parseInt(select) + 1;
+        console.log(rotaSelecionada);
+        updateList(rotaSelecionada);
+        horarioList(select);
+    });
+});
+
+function updateList(rotaSelecionada){ 
+    $.ajax(URL_BASE+"rota/" + rotaSelecionada + "/paradas", {
+        method:'get',
+    }).done(function(res) {
+        console.log(res._embedded.paradaNome);
+        let table = $('#tableContent');
+        table.html("");
+        $(res._embedded.parada).each(function(k,el){
+            let parada = el;
+            tr = $(`<tr><td>${parada.paradaNome}</td></tr>`);
+            console.log(tableContent);
+            table.append(tr);
+        })
+    })
+    .fail(function(res) {
+        let table = $('#tableContent');
+        table.html("");
+        tr = $(`<tr><td colspan='4'>Não foi possível carregar a lista</td></tr>`);
+        table.append(tr);
+    });
+}
+
+function selectList(){
+    $.ajax(URL_BASE+"rota",{
+        method:'get',
+    }).done(function(res) {
+        console.log(res._embedded.rota);
+        let select = $('#rotas-sel');
+        select.html("");
+        $(res._embedded.rota).each(function(index, el){
+            let rota = el;
+            let option =  $('<option></option>').attr('value', index).text(rota.nomeRota);
+            select.append(option);
+        })
+       
+    })
+    .fail(function(res) {
+        let select = $('#rotas-sel');
+        select.html("");
+        let option = $('<option></option>').text('Não foi possível carregar as opções');
+        select.append(option);
+    });
+}
+
+function horarioList(select){
+    $.ajax(URL_BASE+"rota", {
+        method:'get',
+    }).done(function(res) {
+        
+        let table = $('#horario_table');
+        table.html("");
+        let rota = res._embedded.rota[select];
+        console.log(rota);
+        tr = $(`<tr><td>${rota.horaSaida}</td><td>${rota.horaChegada}</td></tr>`);
+        console.log(tableContent);
+        table.append(tr);
+    })
+    .fail(function(res) {
+        let table = $('#horario_table');
+        table.html("");
+        tr = $(`<tr><td colspan='4'>Não foi possível carregar a lista</td></tr>`);
+        table.append(tr);
+    });
+}
